f89a8e3ea5dcceb2d9f1084343c5740d335a51e2
levi2 <levi2@DESKTOP-J245I37>
2021-03-22 11:30:04 +0800

    提交测试更新

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace NoSSLGithubTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World222!");
         }
     }
 }
